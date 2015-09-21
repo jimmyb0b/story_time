5b7de93261ad0672473363dc7ead2afa4b163bd7
James Leisy <jamesleisy@gmail.com>
2015-09-21 15:36:37 -0400

    committing

--- server.js
+++ server.js
@@ -9,24 +9,29 @@ var app = http.createServer(function (req, res) {
 });
 
 var io = require('socket.io').listen(app);
+
 io.sockets.on('connection', function (socket){
 
   // convenience function to log server messages on the client
 	function log(){
-		var array = [];
-		console.log(array)
-	  for (var i = 0; i < users.length; i++) {
-	  	array.push(users[i]);
+		var array = [">>> Message from server: "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
 	  }
 	    socket.emit('log', array);
 	}
 
+	socket.on('message', function (message) {
+		log('Got message:', message);
+    // for a real app, would be room only (not broadcast)
+		socket.broadcast.emit('message', message);
+	});
 
 	socket.on('create or join', function (room) {
 		var numClients = io.sockets.clients(room).length;
 
-		// log('Room ' + room + ' has ' + numClients + ' client(s)');
-		// log('Request to create or join room ' + room);
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room ' + room);
 
 		if (numClients === 0){
 			socket.join(room);
